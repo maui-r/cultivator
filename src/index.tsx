6d480e36e4af72a59f7164b4c19f06cc72a79573
Maui <maui.r@tuta.io>
2022-10-21 13:30:14 +0200

    refactor: disable logs for production build

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,11 @@ import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import App from './App'
 
+if (process.env.NODE_ENV === 'production') {
+  console.debug = () => { }
+  console.log = () => { }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
