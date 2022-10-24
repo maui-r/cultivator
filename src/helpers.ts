897e3c62f1858b30ef379a0e11e0f16c55a648bb
Maui <maui.r@tuta.io>
2022-10-24 13:03:26 +0200

    refactor: use helper functions for hex conversion

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,17 @@
 export const sleep = (milliseconds: number): Promise<void> => {
     return new Promise((resolve) => setTimeout(resolve, milliseconds))
 }
+
+/**
+ * Return even-length hexadecimal string representation of an integer.
+ */
+export const intToHex = (int: number | null): string | null => {
+    if (!int) return null
+    var str = int.toString(16)
+    var evenLenStr = str.length % 2 === 0 ? str : '0'.concat(str)
+    return '0x'.concat(evenLenStr)
+}
+
+export const hexToInt = (hex: string): number => {
+    return parseInt(hex, 16)
+}
