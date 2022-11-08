e13026e7c1f4b6687bb636b68ea6a4bacc2133ef
Maui <maui.r@tuta.io>
2022-11-08 09:16:19 +0100

    feat: add beta warning

---
+++ src/components/Dialog/Beta.tsx
@@ -0,0 +1,40 @@
+import { Link } from '@mui/material'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+import Typography from '@mui/material/Typography'
+import { useAppStore } from '../../stores'
+
+export const BetaDialog = () => {
+  const showBeta = useAppStore((state) => state.showBeta)
+  const setShowBeta = useAppStore((state) => state.setShowBeta)
+
+  const handleClose = () => {
+    setShowBeta(false)
+  }
+
+  return (
+    <Dialog open={showBeta} onClose={handleClose}>
+      <DialogTitle>You're early 🌱</DialogTitle>
+      <DialogContent>
+        <Typography>
+          Cultivator is still in beta. Things may break or behave in unexpected ways.
+          If you find bugs don't keep them all for yourself!
+          Help us improve by creating
+          a <Link href='https://cultivator.canny.io/bug-reports' target='_blank' rel='noopener'>Bug Report</Link> or
+          telling us about
+          the <Link href='https://cultivator.canny.io/feature-requests' target='_blank' rel='noopener'>Feature</Link> you'd
+          like to see the most.
+        </Typography>
+        <Typography>
+        </Typography>
+
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>I understand</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -2,14 +2,17 @@ import { useAppStore, useNodeStore } from '../../stores'
 import HelpDialog from './Help'
 import { AddHandleDialog } from './AddHandle'
 import { SignInDialog } from './SignIn'
+import { BetaDialog } from './Beta'
 
 export const Dialogs = () => {
-    const showHelp = useAppStore((state) => state.showHelp)
-    const showSignIn = useAppStore((state) => state.showSignIn)
-    const nodes = useNodeStore((state) => state.nodes)
+  const showHelp = useAppStore((state) => state.showHelp)
+  const showBeta = useAppStore((state) => state.showBeta)
+  const showSignIn = useAppStore((state) => state.showSignIn)
+  const nodes = useNodeStore((state) => state.nodes)
 
-    if (showHelp) return <HelpDialog />
-    if (showSignIn) return <SignInDialog />
-    if (Object.keys(nodes).length === 0) return <AddHandleDialog />
-    return null
+  if (showHelp) return <HelpDialog />
+  if (showBeta) return <BetaDialog />
+  if (showSignIn) return <SignInDialog />
+  if (Object.keys(nodes).length === 0) return <AddHandleDialog />
+  return null
 }
