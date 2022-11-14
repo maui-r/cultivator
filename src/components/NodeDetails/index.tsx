f40c81306b04c419791b52cf5e839c3a092e4181
Maui <maui.r@tuta.io>
2022-11-14 11:20:47 +0100

    refactor: show info when graph is empty

--- src/components/NodeDetails/index.tsx
+++ src/components/NodeDetails/index.tsx
@@ -1,19 +1,23 @@
 import { Alert, Box, Grid } from '@mui/material'
-import { useAppStore } from '../../stores'
+import shallow from 'zustand/shallow'
+import { useAppStore, useNodeStore } from '../../stores'
 import Profile from './Profile'
 
 export const NodeDetails = () => {
   const selectedNodeId = useAppStore((state) => state.selectedNodeId)
+  const hasNodes = useNodeStore((state) => Object.keys(state.nodes).length > 0, shallow)
+
 
   return <Grid item xs={6} sm={4} md={3} xl={2}>
-    {
-      selectedNodeId
-        ?
-        <Profile profileId={selectedNodeId} />
-        :
-        <Box sx={{ p: 1 }}>
+    {selectedNodeId ?
+      <Profile profileId={selectedNodeId} />
+      :
+      hasNodes ?
+        <Box sx={{ p: 1 }} >
           <Alert severity='info'>Click on a node to show info</Alert>
         </Box>
+        :
+        null
     }
-  </Grid>
+  </Grid >
 }
